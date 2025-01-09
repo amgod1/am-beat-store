35f1d31bceaa4cb875e95e95918f47cb2f348f9e
amgod1 <kamillschepuk@gmail.com>
2025-01-09 16:34:08 +0100

    feat: audio iteraction functions

--- src/modules/Player/components/Player/Player.tsx
+++ src/modules/Player/components/Player/Player.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, useEffect, useRef } from "react"
 import { useAppDispatch, useAppSelector } from "@/hooks"
 import { FaRegCirclePlay } from "react-icons/fa6"
 import { FaRegCirclePause } from "react-icons/fa6"
@@ -13,13 +13,25 @@ import {
 export const Player: FC = () => {
   const info = useAppSelector(selectPlayerInfo)
   const dispatch = useAppDispatch()
+  const currentAudio = useRef<HTMLAudioElement>(null)
 
   const pauseHandler = () => dispatch(pausePlaying())
+
   const continueHandler = () => dispatch(continuePlaying())
+
   const closeHandler = () => dispatch(closePlayer())
+
+  useEffect(() => {
+    if (info.isPlaying && currentAudio.current) {
+      currentAudio.current.play()
+    } else if (!info.isPlaying && currentAudio.current) {
+      currentAudio.current.pause()
+    }
+  }, [info.isPlaying, info.src])
 
   return (
     <div className="my-8 h-8 grid grid-cols-2 sm:grid-cols-3 justify-items-stretch items-center">
+      <audio src={info.src!} ref={currentAudio} />
       <h2 className="text-base sm:text-xl">{info.title}</h2>
       {info.isPlaying ? (
         <button
